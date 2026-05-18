045f400a96e66d1cb90953a4e11e8c936d5c865b
原点 <467490186@qq.com>
2026-05-18 14:17:37 +0800

    Update BeanUtil.php

--- src/bean/BeanUtil.php
+++ src/bean/BeanUtil.php
@@ -19,12 +19,43 @@ use yuandian\Tools\bean\mapper\ObjectToObjectMapper;
 
 /**
  * Bean 工具类
+ * @template T of object
  */
 class BeanUtil
 {
     /**
+     * 按照Bean对象属性创建对应的Class对象
+     * @param array|object $from
+     * @param class-string<T> $to
+     * @return T|T[]
+     * @date 2026/5/18 上午11:05
+     * @author 原点 467490186@qq.com
+     */
+    public static function copyProperties(array|object|string $from, string|object $to): object|array
+    {
+        if (is_object($from)) {
+            return (new ObjectToObjectMapper())->map($from, $to);
+        }
+        if (array_is_list($from)) {
+            $data = [];
+            foreach ($from as $item) {
+                $data[] = (new ArrayToObjectMapper())->map($item, $to);
+            }
+            return $data;
+        }
+        if (is_array($from)) {
+            return (new ArrayToObjectMapper())->map($from, $to);
+        }
+        if (is_string($from)) {
+            if (function_exists("json_validate") && !json_validate($from)) {
+                throw new \Exception("json_validate failed");
+            }
+            return (new ArrayToObjectMapper())->map(json_decode($from, true), $to);
+        }
+    }
+
+    /**
      * 数组转对象
-     * @template T of object
      * @param array $from
      * @param class-string<T> $to
      * @return T
@@ -39,10 +70,9 @@ class BeanUtil
 
     /**
      * 数组转对象列表
-     * @template ItemObject of object
      * @param array $from
-     * @param class-string<ItemObject>|ItemObject $to
-     * @return array<ItemObject>
+     * @param class-string<T>|T $to
+     * @return T[]
      * @throws \ReflectionException
      * @date 2025/7/21 上午11:04
      * @author 原点 467490186@qq.com
@@ -71,9 +101,8 @@ class BeanUtil
 
     /**
      * 对象转对象
-     * @template T of object
      * @param object $from
-     * @param class-string<T> $to
+     * @param class-string<T>|T $to
      * @return T
      * @throws \ReflectionException
      * @date 2025/7/21 上午11:14
@@ -97,7 +126,7 @@ class BeanUtil
      */
     public static function jsonToObject(string $from, string|object $to): object
     {
-        if(function_exists("json_validate") &&  !json_validate($from)){
+        if (function_exists("json_validate") && !json_validate($from)) {
             throw new \Exception("json_validate failed");
         }
         return (new ArrayToObjectMapper())->map(json_decode($from, true), $to);
