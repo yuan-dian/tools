736ca41ea0e5de0cd2fc3bb7b71cfa08becd63bd
原点 <467490186@qq.com>
2026-07-09 11:50:45 +0800

    feat：增加string常量的常量池

---
+++ src/const/StringPoolConst.php
@@ -0,0 +1,73 @@
+<?php
+// +----------------------------------------------------------------------
+// | 
+// +----------------------------------------------------------------------
+// | @copyright (c) 原点 All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 原点 <467490186@qq.com>
+// +----------------------------------------------------------------------
+// | Date: 2026/7/3
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
+
+namespace yuandian\Tools\const;
+
+interface StringPoolConst
+{
+    public const AMPERSAND = "&";
+    public const AND = "and";
+    public const AT = "@";
+    public const ASTERISK = "*";
+    public const STAR = self::ASTERISK;
+    public const SLASH = "/";
+    public const BACK_SLASH = '\\';
+    public const DOUBLE_SLASH = "#//";
+    public const COLON = ":";
+    public const COMMA = ",";
+    public const DASH = "-";
+    public const DOLLAR = "$";
+    public const DOT = ".";
+    public const EMPTY = "";
+    public const EMPTY_JSON = "{}";
+    public const EQUALS = "=";
+    public const FALSE = "false";
+    public const HASH = "#";
+    public const HAT = "^";
+    public const LEFT_BRACE = "{";
+    public const LEFT_BRACKET = "(";
+    public const LEFT_CHEV = "<";
+    public const NEWLINE = "\n";
+    public const N = "n";
+    public const NO = "no";
+    public const NULL = "null";
+    public const OFF = "off";
+    public const ON = "on";
+    public const PERCENT = "%";
+    public const PIPE = "|";
+    public const PLUS = "+";
+    public const QUESTION_MARK = "?";
+    public const EXCLAMATION_MARK = "!";
+    public const  QUOTE = "\"";
+    public const RETURN = "\r";
+    public const TAB = "\t";
+    public const RIGHT_BRACE = "}";
+    public const RIGHT_BRACKET = ")";
+    public const RIGHT_CHEV = ">";
+    public const SEMICOLON = ";";
+    public const SINGLE_QUOTE = "'";
+    public const BACKTICK = "`";
+    public const SPACE = " ";
+    public const TILDA = "~";
+    public const LEFT_SQ_BRACKET = "[";
+    public const RIGHT_SQ_BRACKET = "]";
+    public const TRUE = "true";
+    public const UNDERSCORE = "_";
+    public const UTF_8 = "UTF-8";
+    public const GBK = "GBK";
+    public const ISO_8859_1 = "ISO-8859-1";
+    public const Y = "y";
+    public const YES = "yes";
+    public const ONE = "1";
+    public const ZERO = "0";
+}
